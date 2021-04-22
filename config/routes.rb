066ba72347d4108d1b5e4431f9587b3b89adcbda
naokihayasan127 <65150371+naokihayasan127@users.noreply.github.com>
2021-04-22 10:59:13 +0900

    コメント機能の本番環境修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
   resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
-    resources :comments, only: :create
+    resources :comments, only: :create 
   end
   resources :users, only: :show
 end
+
+#If no route matches
+match ":url" => "application#redirect_user", :constraints => { :url => /.*/ }
